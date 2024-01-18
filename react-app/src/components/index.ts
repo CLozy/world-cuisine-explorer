e2974d2844ae19534d76cecaba73ec1e71c079c9
Charleenlozi <clozymwangs@gmail.com>
2024-01-18 05:40:33 +0300

    feat: adding breakingpoint for responsive design

---
+++ react-app/src/components/index.ts
@@ -0,0 +1,3 @@
+//import more components over one line
+
+export { default as CuisinePage} from './CuisinePage';
--- react-app/tailwind.config.js
+++ react-app/tailwind.config.js
@@ -2,7 +2,41 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      // to center text on mobile, and left align it on screens 640px and wider use <div class="text-center sm:text-left"></div>
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
+    colors: {
+      blue: "#1fb6ff",
+      purple: "#7e5bef",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
   },
   plugins: [],
 };
